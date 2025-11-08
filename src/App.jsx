6589d4c99befde11ffd79bf849d37d13e95169b9
cheangdominic <dcheang@my.bcit.ca>
2025-11-07 19:22:19 -0800

    Setup React.js + Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Tailwind is Working 🎉</h1>
+    </div>
+  );
+}
