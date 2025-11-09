e5a3d1e20d43e2d5ad4a12fd243f32ee55e3cf86
Jbalfour5 <jbalfour5@my.bcit.ca>
2025-11-09 09:15:30 -0800

    Removed unused files

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold">Tailwind is Working 🎉</h1>
-    </div>
-  );
-}
